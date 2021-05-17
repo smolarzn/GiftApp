20dc8be924ba42f8b952524528ea5cabe960124d
smolarzn <04650nat@gmail.com>
2021-05-17 10:55:19 +0200

    GiftAppStarterApplication class added

---
+++ src/main/java/com/natalia/domain/GiftAppStarterApplication.java
@@ -0,0 +1,12 @@
+package com.natalia.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftAppStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftAppStarterApplication.class, args);
+    }
+}
