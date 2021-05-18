898f4d5abf5c1a2b5225795902df077d06d54061
smolarzn <04650nat@gmail.com>
2021-05-18 10:19:37 +0200

    HappeningEntity implemented

---
+++ src/main/java/com/natalia/domain/HappeningEntity.java
@@ -0,0 +1,61 @@
+package com.natalia.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "happening")
+public class HappeningEntity extends AbstractEntity {
+
+    @NotNull
+    private String topic;
+
+    @Size(min = 5, max = 50)
+    private String description;
+
+    @NotNull
+    private LocalDate time;
+
+    @ManyToMany
+    @JoinTable(
+            name = "happening_to_participant",
+            joinColumns = @JoinColumn(name = "happening_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<UserEntity> participants;
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDate time) {
+        this.time = time;
+    }
+
+    public List<UserEntity> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<UserEntity> participants) {
+        this.participants = participants;
+    }
+}
