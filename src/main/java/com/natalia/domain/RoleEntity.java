8c8154b9bfd014ec63eaca7feb1b7933465741fc
smolarzn <04650nat@gmail.com>
2021-06-08 15:40:25 +0200

    user-role relation created

--- src/main/java/com/natalia/domain/RoleEntity.java
+++ src/main/java/com/natalia/domain/RoleEntity.java
@@ -1,11 +1,9 @@
 package com.natalia.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "role")
 public class RoleEntity {
 
     @Id
--- src/main/java/com/natalia/domain/UserEntity.java
+++ src/main/java/com/natalia/domain/UserEntity.java
@@ -2,11 +2,10 @@ package com.natalia.domain;
 
 import com.natalia.domain.validator.Password;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
+import java.util.Set;
 
 @Entity
 @Table(name = "app_user")
@@ -21,6 +20,11 @@ public class UserEntity extends AbstractEntity {
 
     @Email(regexp = "[_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.([a-zA-Z]{2,}){1}")
     private String email;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<RoleEntity> roles;
 
     public String getUserName() {
         return userName;
@@ -44,5 +48,13 @@ public class UserEntity extends AbstractEntity {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Set<RoleEntity> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<RoleEntity> roles) {
+        this.roles = roles;
     }
 }
