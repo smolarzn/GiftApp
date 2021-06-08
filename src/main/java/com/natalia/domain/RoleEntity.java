5845110d93d54653382ba4ef84ab958c07d6d2f1
smolarzn <04650nat@gmail.com>
2021-06-08 15:22:45 +0200

    RoleEntity created

---
+++ src/main/java/com/natalia/domain/RoleEntity.java
@@ -0,0 +1,24 @@
+package com.natalia.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
