65baeaa8989681c873b80c1a86b09e59e4f0fd70
smolarzn <04650nat@gmail.com>
2021-06-09 14:13:51 +0200

    application-hsql.properties for tests created; UserServiceImplementationTest -> data.sql path addjusted

--- src/test/java/com/natalia/services/impl/UserServiceImplementationTest.java
+++ src/test/java/com/natalia/services/impl/UserServiceImplementationTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
@@ -17,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @Transactional
+@Sql("/inserts/data.sql")
 public class UserServiceImplementationTest {
 
     @Autowired
